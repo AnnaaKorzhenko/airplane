c28472fa165f267d6418e4532efe1b6b1971d6fd
Anna Korzhenko <akorzhenko@kse.org.ua>
2024-09-05 18:35:34 +0300

    first attempt of classes design

--- main.cpp
+++ main.cpp
@@ -1,2 +1,35 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+class Flight {
+private:
+    string flightID;
+    int seatsPerRow;
+    string date;
+};
+
+class Ticket {
+private:
+    string passengerName;
+    int passengerID;
+    string seat;
+    string flightID;
+    string date;
+
+};
+class Seat {
+private:
+    string flightID;
+    int row;
+    char place;
+    bool isAvailable;
+
+};
+class ParserFromBase{};
+class ParserFromUser{};
+
+
+int main() {
+    return 0;
+}
